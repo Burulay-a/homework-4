909d9e60111f36c6f5a46e38dda425989579e4ae
Burulay-a <abrly6711@gmail.com>
2022-10-11 21:38:57 +0600

    div

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css'
+
+function Card(){
+
+    return(
+        <div className='div'>HELLO WORLD!!!</div>
+    )
+}
+
+export default Card;
---
+++ src/components/Cardtwo.jsx
@@ -0,0 +1,11 @@
+
+
+function Cardtwo(){
+
+    return(
+
+     <div className="secondDiv">HELLO WORLD!!!!</div>
+    )
+}
+
+export default Cardtwo
